24d66c739b5a8f74a47df8180e43d7997b2abbb6
fregulationn <zjj123ily@sina.com>
2018-07-26 17:51:27 +0800

    Create __init__.py

---
+++ setting/__init__.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+File Name：     __init__.py
+Author :       peng.he
+-------------------------------------------------
+"""
+
+
+class ModelType():
+    LR = 'logistic regression',
+    # 未实现
+    CNN = 'convolutional neural network'
