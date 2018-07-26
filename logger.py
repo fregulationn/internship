5e662c811dcd9b37b51b32a493c2129e009aeb00
fregulationn <zjj123ily@sina.com>
2018-07-26 16:20:11 +0800

    Create logger.py

---
+++ logger.py
@@ -0,0 +1,49 @@
+# -*- coding: utf-8 -*-
+"""
+File Name：     logger
+Author :       peng.he
+-------------------------------------------------
+"""
+import logging.config
+from sentence import settings
+
+LOG_PATH = settings.LOG_PATH
+
+LOGGING_CONFIG = {
+    'version': 1,
+    'disable_existing_loggers': True,
+    'loggers': {
+        'file': {
+            'level': 'INFO',
+            'handlers': ['file'],
+            'propagate': False
+        },
+        'werkzeug': {
+            'level': 'INFO',
+            'handlers': ['file'],
+            'propagate': False
+        },
+    },
+    'handlers': {
+        'file': {
+            'level': 'INFO',
+            'class': 'logging.handlers.TimedRotatingFileHandler',
+            'when': 'D',
+            'backupCount': 30,
+            'filename': LOG_PATH,
+            'formatter': 'verbose',
+        }
+    },
+    'formatters': {
+        'verbose': {
+            'format':
+                '%(asctime)s %(levelname)s:%(name)s:%(lineno)d: %(message)s',
+            'datefmt': '%Y-%m-%d %H:%M:%S'
+        },
+    }
+}
+
+
+def init_logger():
+    """Init logger instance."""
+    logging.config.dictConfig(LOGGING_CONFIG)
